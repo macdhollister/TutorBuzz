e7b14e2b67d40c4bd84ff9f52b722ff501fec1e2
macdh <macdhollister@gmail.com>
2019-03-24 02:01:25 -0700

    code spacing issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
-import Homepage from "./pages/homepage";
+import Homepage from "./pages/Homepage";
 // import Footer from "./components/Footer";
 import tutorPortal from "./pages/tutorPortal.js";
 import studentPortal from "./pages/studentPortal.js";
@@ -10,14 +10,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-        <Router>
-            <div className="App">
-                <Navbar />
-                <Route exact path="/" component={Homepage} />
-                <Route exact path="/tutorPortal" component={tutorPortal} />
-                <Route exact path="/studentPortal" component={studentPortal} />
-            </div>
-        </Router>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/tutorPortal" component={tutorPortal} />
+          <Route exact path="/studentPortal" component={studentPortal} />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,13 +4,11 @@ import "./Footer.css";
 class Footer extends Component {
   render() {
     return (
-        <footer>
-            <div class="has-text-centered footerDiv">
-                <p>
-                    <strong class="footerText">TutorBuzz</strong> © 2019 by <p>Josie Strange, Ana Lee, Mac Hollister, Jonathan Davis, and Garrett Straight</p>
-                </p>
-            </div>
-        </footer>
+      <footer>
+        <div className="has-text-centered footerDiv">
+          <strong className="footerText">TutorBuzz</strong> © 2019 by <p>Josie Strange, Ana Lee, Mac Hollister, Jonathan Davis, and Garrett Straight</p>
+        </div>
+      </footer>
     );
   }
 }
