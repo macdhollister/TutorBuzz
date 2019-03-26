67d30749952a61cab46f9704881633255349003f
Garrett Straight <41503255+GarrettStraight@users.noreply.github.com>
2019-03-25 19:47:36 -0700

    Map components added skeleton

---
+++ client/src/components/Map/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import "./Map.css";
+
+class Map extends Component {
+    render() {
+        return (
+            <div className="box" id="studentMap">
+                <article className="media">
+                    <div className="media-content">
+                        <div className="content">
+                        <p className="is-size-2">Student's Map Goes Here</p>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        );
+    }
+}
+
+export default Map;
+
