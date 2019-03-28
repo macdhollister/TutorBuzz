639947982902b8079e9e68ffd7a69035c17129aa
macdh <macdhollister@gmail.com>
2019-03-28 00:56:13 -0700

    added config for sequelize

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": process.env.DB_PASSWORD,
+    "database": "tutorbuzz",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "logging": false
+  },
+  "test": {
+    "username": "root",
+    "password": process.env.DB_PASSWORD,
+    "database": "tutorbuzz",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "logging": false
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+}
