ec498dfdddd971b848800abf65fd4b09c8df6aeb
macdh <macdhollister@gmail.com>
2019-03-28 00:54:28 -0700

    added placeholder sequelize Tutor model

--- server/models/Tutor.js
+++ server/models/Tutor.js
@@ -1,35 +1,57 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
 
-var tutorSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
+// var TutorSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
 
-  email: {
-    type: String,
-    required: true
-  },
+//   email: {
+//     type: String,
+//     required: true
+//   },
 
-  unavailable: {
-    type: String,
-    required: true
-  },
+//   unavailable: {
+//     type: String,
+//     required: true
+//   },
 
-  students: {
-    type: [Schema.Types.ObjectId],
-    ref: "Students"
-},
+//   students: {
+//     type: [Schema.Types.ObjectId],
+//     ref: "Students"
+//   },
 
-sessions: {
-  type: [Schema.Types.ObjectId],
-  ref: "Session"
+//   sessions: {
+//     type: [Schema.Types.ObjectId],
+//     ref: "Session"
+//   }
+// });
+
+// const Tutor = mongoose.model("Tutor", TutorSchema);
+// module.exports = Tutor;
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("Test", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        // isStudent field? 
+        // association to Tutor/Student model?
+    }, {
+        freezeTableName: true
+    });
+
+    return User;
 }
-
-}
-});
-
-const Tutor = mongoose.model("Tutor", TutorSchema);
-module.exports = Tutor;
-
