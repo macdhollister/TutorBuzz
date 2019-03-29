3bec35d6ff349120ff6160f10e4719284b345445
Josie Strange <42877190+Jostrange@users.noreply.github.com>
2019-03-28 19:21:28 -0700

    Create student.js

---
+++ server/models/student.js
@@ -0,0 +1,35 @@
+module.exports = function (sequelize, DataTypes) {
+  var offers = sequelize.define("student", {
+    time: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    day: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    location: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    student: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    tutor: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  });
+
+  student.associate = function(models) {
+    student.belongsTo(models.tutor, {
+      // foreignKey: "email_id",
+      onDelete: "cascade"
+    });
+  };
+
+  return student;
+};
+
