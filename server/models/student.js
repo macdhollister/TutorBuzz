8b72ecaa421d317ba9178b297365f0797abd2d0f
Josie Strange <42877190+Jostrange@users.noreply.github.com>
2019-03-28 19:28:46 -0700

    Update student.js

--- server/models/student.js
+++ server/models/student.js
@@ -1,5 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
-  var offers = sequelize.define("student", {
+  var student = sequelize.define("student", {
     time: {
       type: DataTypes.INTEGER,
       allowNull: false
