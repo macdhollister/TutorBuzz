6719c200965d485f5411fa6100ce7825ca100c00
macdh <macdhollister@gmail.com>
2019-04-01 22:19:41 -0700

    Created login and JsonWebToken models in passport config

---
+++ server/config/jwtConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    /*
+     * Note: In a real production level project, this would be an environment variable
+     * doing it this way only for simplicity and time's sake to have a
+     * "proof of concept" before proejct presentations
+     */
+    secret: "super-duper secret secret"
+}
