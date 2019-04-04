17bd90b45aafddc60b58a92e66f68f1d283d351c
Mac Hollister <macdhollister@gmail.com>
2019-04-03 22:37:15 -0700

    User signup (#74)
    * deleted server-old
    
    * deleted unused files
    
    * updated passport config (still needs to be tested)
    
    * updated database models
    
    * added logic to sign up new users (POSTMAN ONLY FOR NOW)
    
    * small server changes

--- server-old/models/index.js
+++
@@ -1,7 +0,0 @@
-const Session = require("./Session");
-const Student = require("./Student");
-const User = require("./User");
-
-module.exports = {
-    Session, Student, User
-}
--- server/models/Student.js
+++ server/models/Student.js
@@ -1,38 +1,41 @@
 module.exports = (sequelize, DataTypes) => {
-  const Student = sequelize.define("Student", {
-      email: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-          validate: {
-              isEmail: true
-          }
-      },
+    const Student = sequelize.define("Student", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
 
-      name: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+      
+        // tutors: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     defaultValue: ""
+        // },
+      
+        // sessions: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     defaultValue: ""
+        // }
+    }, {
+        freezeTableName: true
+    });
 
-      password: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-      
-      tutors: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          defaultValue: ""
-      },
-      
-      sessions: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          defaultValue: ""
-      }
-  }, {
-      freezeTableName: true
-  });
+    Student.associate = function(models) {
+        Student.hasOne(models.User, {
+            onDelete: "cascade",
+            foreignKey: "studentId",
+            sourceKey: "id"
+        })
+    }
 
   return Student;
 }
