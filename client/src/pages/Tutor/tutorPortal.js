a7b72d4d272c9e46fc7b639fd38ec80ddc4f1bb1
macdh <macdhollister@gmail.com>
2019-04-04 17:50:30 -0700

    added componentdidmount to tutorPortal

--- client/src/pages/Tutor/tutorPortal.js
+++ client/src/pages/Tutor/tutorPortal.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import fetch from 'node-fetch';
+
 import Footer from '../../components/StaticComps/Footer';
 import TutorGreeting from '../../components/TutorComps/TutorGreeting';
 import TutorCalendar from '../../components/TutorComps/TutorCalendar';
@@ -10,29 +12,37 @@ import TutorRequests from '../../components/TutorComps/TutorRequests';
 import './tutorPortal.css';
 
 class tutorPortal extends Component {
+  componentDidMount() {
+    fetch("/selfDataTutor")
+    .then(res => res.json())
+    .then(res => console.log(res));
+  }
+
+
+
   render() {
     return (
 
       <body className="Site">
-     <div className="columns">
-        <div className="column">
-          <TutorGreeting />
-          <TutorTodaySess />
-          <TutorSessions />
+        <div className="columns">
+          <div className="column">
+            <TutorGreeting />
+            <TutorTodaySess />
+            <TutorSessions />
+          </div>
+          <div className="column">
+            <TutorCalendar />
+          </div>
+          <div className="column">
+            <TutorTimeCard />
+            <TutorAvailability />
+            <TutorRequests />
+          </div>
         </div>
-        <div className="column">
-          <TutorCalendar />
-        </div>
-        <div className="column">
-          <TutorTimeCard />
-          <TutorAvailability />
-          <TutorRequests />
-        </div>
-      </div>
 
-      <div id="footerFixTutor"></div>
-      
-      <Footer />
+        <div id="footerFixTutor"></div>
+
+        <Footer />
 
       </body>
 
