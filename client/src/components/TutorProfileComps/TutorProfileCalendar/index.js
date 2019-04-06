a032148cdf2c9d0050e9fc0368bb6f895a83d84e
macdh <macdhollister@gmail.com>
2019-04-06 07:25:35 -0700

    Sessions render on calendar on Tutor Profile page

--- client/src/components/TutorProfileComps/TutorProfileCalendar/index.js
+++ client/src/components/TutorProfileComps/TutorProfileCalendar/index.js
@@ -2,35 +2,28 @@ import React, { Component } from 'react';
 import "./TutorProfileCalendar.css";
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
+import dateToDayPicker from "../../../utils/dateToDayPicker"
 
 export class TutorProfileCalendar extends Component {
-
-  // fetch('/sessions')
-  //   .then(function(response) {
-  //     // Modify data here
-  // })
-  // .catch(function(error){
-  //   console.log(error);
-  // });
 
   constructor(props) {
     super(props);
 
     this.state = {
-      sessions: [
-        {
-          sessionDate: 'new Date(2019, 2, 12)'
-        },
-        {
-          sessionDate: 'new Date(2019, 2, 6)'
-        },
-        {
-          sessionDate: 'new Date(2019, 2, 18)'
-        },
-        {
-          sessionDate: 'new Date(2019, 2, 27)'
-        }
-      ]
+      sessions: []
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.data !== this.props.data) {
+      const data = this.props.data;
+      const formattedData = data.map(sessData => {
+        return new Date(...dateToDayPicker(sessData.day));
+      })
+
+      this.setState({
+        sessions: formattedData
+      })
     }
   }
 
