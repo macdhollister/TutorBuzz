0fc5a7a57292a51c6f68cbb19374062632706c81
macdh <macdhollister@gmail.com>
2019-04-08 17:40:05 -0700

    Root directory package.json

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const session = require("express-session");
 const PORT = process.env.PORT || 3001;
 const httpServer = require("http").Server(app);
+const path = require("path");
 
 // Passport config
 const passport = require("passport");
@@ -36,7 +37,13 @@ app.use((req, res, next) => {
 app.use(require("./routes/authRoutes"));
 app.use(require("./routes/apiRoutes"));
 
-    // add other routes here
+const clientFolder = path.join(__dirname, "../client/build")
+console.log("serving assets from client folder: " + clientFolder);
+app.use(express.static(clientFolder));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(clientFolder, "index.html"))
+})
 
 db.sequelize.sync().then(() => {
     httpServer.listen(PORT, console.log(`Server listening on PORT ${PORT}`));
